12d6c4c649cd8e824281b74ed0b283dbacb13445
Sonun <usubalievasonun@gmail.com>
2023-03-19 17:51:50 +0600

    Initial Commit

---
+++ PracticalTask03/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticalTask03
+//
+//  Created by Sonun on 19/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
